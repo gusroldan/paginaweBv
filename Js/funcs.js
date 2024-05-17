e2e93baaff193d51084e361a51639d76f1b0eaca
gusroldan <134972575+gusroldan@users.noreply.github.com>
2024-05-17 09:16:52 -0400

    Update funcs.js

--- Js/funcs.js
+++ Js/funcs.js
@@ -1,48 +1,53 @@
 $(document).ready(function () {
+  
   $("#subir").click(function () {
     var nombre = $("#nombre").val();
     var correo = $("#correo").val();
     var numero = $("#numero").val();
     var ocupacion = $("#ocupacion").val();
-
-    // Validación del campo nombre
-    if (nombre.trim() == "") {
+    if (nombre == "") {
       alert("El campo nombre es obligatorio.");
+
       return false;
-    } else if (nombre.length < 3) {
+    }
+    else if (nombre.length < 3) {
       alert("El nombre debe tener al menos 3 caracteres.");
+      
       return false;
     }
-
-    // Validación del campo correo
-    if (correo.trim() == "") {
+    if (correo == "") {
       alert("El campo correo es obligatorio.");
+
       return false;
-    } else if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(correo)) {
+    }
+    else if (correo.length < 3) {
+      alert("El correo debe tener al menos 3 caracteres.");
+
+      return false;
+    }
+    if (numero == "") {
+      alert("El campo numero es obligatorio.");
+
+      return false;
+    }
+    if (ocupacion == "") {
+      alert("El campo ocupacion es obligatorio.");
+
+      return false;
+    }
+    else if (ocupacion.length < 10) {
+      alert("El ocupacion debe tener al menos 10 caracteres.");
+
+      return false;
+    }
+    var correoRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (!correoRegex.test(correo)) {
       alert("Por favor, ingrese un correo electrónico válido.");
+
       return false;
     }
-
-    // Validación del campo número
-    if (numero.trim() == "") {
-      alert("El campo número es obligatorio.");
-      return false;
-    } else if (numero.length < 3) {
-      alert("El número debe tener al menos 3 dígitos.");
-      return false;
-    }
-
-    // Validación del campo ocupación
-    if (ocupacion.trim() == "") {
-      alert("El campo ocupación es obligatorio.");
-      return false;
-    } else if (ocupacion.length < 10) {
-      alert("La ocupación debe tener al menos 10 caracteres.");
-      return false;
-    }
-
-    // Si todas las validaciones son exitosas, se muestra un mensaje de éxito
-    alert("El formulario se envió exitosamente");
+    alert("El formulario se envio exitosamente");
     return true;
   });
 });
